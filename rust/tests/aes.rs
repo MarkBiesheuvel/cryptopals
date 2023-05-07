06bf35169f43e05e10155292256405f7669b5996
Mark Biesheuvel <mail@markbiesheuvel.nl>
2023-05-07 09:33:22 +0200

    Add more test cases

--- rust/tests/aes.rs
+++ rust/tests/aes.rs
@@ -13,7 +13,7 @@ fn roundkey() {
         "D2 60 0D E7 15 7A BC 68 63 39 E9 01 C3 03 1E FB",
         "A1 12 02 C9 B4 68 BE A1 D7 51 57 A0 14 52 49 5B",
         "B1 29 3B 33 05 41 85 92 D2 10 D2 32 C6 42 9B 69",
-        "BD 3D C2 87 B8 7C 47 15 6A 6C 95 27 AC 2E 0E 4E",
+        "BD 3D C2 87 B8 7C 47 15 6A 6C 95 27 AC 2E 0E 4E", // Mistake in PDF
         "CC 96 ED 16 74 EA AA 03 1E 86 3F 24 B2 A8 31 6A",
         "8E 51 EF 21 FA BB 45 22 E4 3D 7A 06 56 95 4B 6C",
         "BF E2 BF 90 45 59 FA B2 A1 64 80 B4 F7 F1 CB D8",
@@ -40,6 +40,11 @@ fn cipher() {
         "63 C0 AB 20 EB 2F 30 CB 9F 93 AF 2B A0 92 C7 A2",
         "63 2F AF A2 EB 93 C7 20 9F 92 AB CB A0 C0 30 2B",
         "BA 75 F4 7A 84 A4 8D 32 E8 8D 06 0E 1B 40 7D 5D",
+        "58 47 08 8B 15 B6 1C BA 59 D4 E2 E8 CD 39 DF CE",
+        "6A A0 30 3D 59 4E 9C F4 CB 48 98 9B BD 12 9E 8B",
+        "6A 4E 98 8B 59 48 9E 3D CB 12 30 F4 BD A0 9C 9B", // Mistake in PDF
+        "15 CE 89 65 C9 4D 71 47 7F 4B BE 97 9D C2 88 CD",
+        "43 C6 A9 62 0E 57 C0 C8 09 08 EB FE 3D F8 7F 37",
     ]
     .into_iter()
     .map(|string| Hexadecimal::from(string))
@@ -63,5 +68,25 @@ fn cipher() {
 
     // Round 1 - Mix columns
     state.mix_columns();
+    assert_eq!(state, expected_states.next().unwrap());
+
+    // Round 1 - Apply roundkey
+    state ^= key.next().unwrap();
+    assert_eq!(state, expected_states.next().unwrap());
+
+    // Round 2 - Substitution bytes
+    state.sub_bytes();
+    assert_eq!(state, expected_states.next().unwrap());
+
+    // Round 2 - Shift rows
+    state.shift_rows();
+    assert_eq!(state, expected_states.next().unwrap());
+
+    // Round 2 - Mix columns
+    state.mix_columns();
+    assert_eq!(state, expected_states.next().unwrap());
+
+    // Round 2 - Apply roundkey
+    state ^= key.next().unwrap();
     assert_eq!(state, expected_states.next().unwrap());
 }
