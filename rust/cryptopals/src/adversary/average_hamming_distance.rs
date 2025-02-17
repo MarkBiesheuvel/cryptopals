80f87d357827e0cb4d8265a90f24183b36be2ea9
Mark Biesheuvel <mail@markbiesheuvel.nl>
2025-02-17 21:41:24 +0100

    Replace last use of `itertools` with `itermore`

--- rust/cryptopals/src/adversary/average_hamming_distance.rs
+++ rust/cryptopals/src/adversary/average_hamming_distance.rs
@@ -1,10 +1,8 @@
-use std::rc::Rc;
-
-use error_stack::{ensure, Result};
-use itertools::Itertools;
-
 use super::AdversaryError;
 use crate::byte::*;
+use error_stack::{ensure, Result};
+use itermore::IterArrayCombinations;
+use std::rc::Rc;
 
 // The maximum number of blocks to sample and compare to each other to calculate
 // the average hamming distance.
@@ -21,7 +19,7 @@ pub fn average_hamming_distance(bytes: &ByteSlice, block_size: usize) -> Result<
         .collect::<Vec<_>>();
 
     // Create all possible combinations
-    let combinations = blocks.into_iter().tuple_combinations().collect::<Vec<_>>();
+    let combinations = blocks.into_iter().array_combinations().collect::<Vec<_>>();
 
     // Count the actual number of combinations
     let number_of_combinations = combinations.len();
@@ -34,7 +32,7 @@ pub fn average_hamming_distance(bytes: &ByteSlice, block_size: usize) -> Result<
     let total_distance = combinations
         .into_iter()
         // Calculate hamming distance for each combination
-        .map(|(block_1, block_2)| block_1.hamming_distance(&block_2).unwrap())
+        .map(|[block_1, block_2]| block_1.hamming_distance(&block_2).unwrap())
         // Sum over all combination
         .sum::<usize>();
 
